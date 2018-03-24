47af4d8e7062f4507dc527016112e3b73e4ee4cc
aawadall <aawadall@ualberta.ca>
2018-03-24 15:46:38 -0400

    fixed learning algorithm

--- js/rl.js
+++ js/rl.js
@@ -3,20 +3,20 @@
 /**
  * Re-infocement Learning Sandbox
  * Copyrights Ahmed Awadallah 2018
- * 
+ *
  */
 
  // TODO: Build Policy
 var policy = {};
-policy.Q = []; // Empty Action/State value pair matrix 
+policy.Q = []; // Empty Action/State value pair matrix
 policy.epsilon = 1e-5; // Exploration Rate
 policy.alpha = 0.3; // Learning Rate
 policy.gamma = 0.9; // Discount Rate
 
-// Q Constructor 
+// Q Constructor
 policy.buildQ = function(states, actions){
     this.Q = new Array(states);
-    
+
    for (var i=0;i < states;i++){
        this.Q[i] = new Array(actions);
         for (var j=0; j< actions; j++){
@@ -32,18 +32,17 @@ policy.bestMove = function(state){
     let stateQ = this.Q[state];
     return stateQ.indexOf(Math.max(stateQ));
 };
-policy.learn = function(state1, state2, action, reward){ 
-    this.Q[state1, action] = (1 - this.alpha) * this.Q[state1, action] +
+policy.learn = function(state1, state2, action, reward){
+    this.Q[state1][action] = (1 - this.alpha) * this.Q[state1][action] +
                               this.alpha * (reward + this.gamma * this.maxValue(state2));
 };
 
  // TODO: Build Agent
- // TODO: Build Environment 
+ // TODO: Build Environment
 
- // Main App 
+ // Main App
  var states = 5;
  var actions = 3;
  policy.buildQ(states, actions);
 
 
- 
