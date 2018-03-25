2026e4fd95a97253d25011d4ad6e39dad2effa9b
aawadall <aawadall@ualberta.ca>
2018-03-25 13:17:34 -0400

    replaced policy with agent object including policy and used factory method to create objects

--- js/rl.js
+++ js/rl.js
@@ -6,43 +6,56 @@
  *
  */
 
- // TODO: Build Policy
-var policy = {};
-policy.Q = []; // Empty Action/State value pair matrix
-policy.epsilon = 1e-5; // Exploration Rate
-policy.alpha = 0.3; // Learning Rate
-policy.gamma = 0.9; // Discount Rate
-
-// Q Constructor
-policy.buildQ = function(states, actions){
-    this.Q = new Array(states);
-
-   for (var i=0;i < states;i++){
-       this.Q[i] = new Array(actions);
-        for (var j=0; j< actions; j++){
-            this.Q[i][j] = 0.0;
+ /**
+  * create new agent object
+  */
+var createAgent = function(numStates, numActions){
+    var _learningRate = 0.3; // Default learning rate (alpha)
+    var _discountRate = 0.99; // Default discount rate (gamma)
+    var _explorationChance = 1e-5; // Probability of exploring beyond policy (epsilon)
+    var _Q = new Array(numStates);
+    for (var i=0; i < numStates; i++){
+        _Q[i] = new Array(numActions);
         }
+
+    // Methods
+    var _bestValue = function(state){
+        return Math.max(_Q[state]);
     }
-};
-// Policy Methods
-policy.maxValue = function(state){
-    return Math.max(this.Q[state]);
-}
-policy.bestMove = function(state){
-    let stateQ = this.Q[state];
-    return stateQ.indexOf(Math.max(stateQ));
-};
-policy.learn = function(state1, state2, action, reward){
-    this.Q[state1][action] = (1 - this.alpha) * this.Q[state1][action] +
-                              this.alpha * (reward + this.gamma * this.maxValue(state2));
+
+    var _bestMove = function(state){
+        let stateQ = _Q[state];
+        return stateQ.indexOf(Math.max(stateQ));
+    };
+
+    var _learn = function(state1, state2, action, reward){
+        _Q[state1][action] = (1 - alpha) * _Q[state1][action] +
+                              alpha * (reward + gamma * maxValue(state2));
+    };
+
+    var _nextMove = function(state){
+        if (Math.random() < _explorationChance){
+            return Math.floor(Max.random() * numActions);
+        } else {
+            return _bestMove(state);
+        }
+    };
+    return {
+        alpha: _learningRate,
+        gamma: _discountRate,
+        epsilon: _explorationChance,
+        Q: _Q,
+        learn: _learn,
+        _nextMove: _nextMove
+    };
 };
 
- // TODO: Build Agent
+
  // TODO: Build Environment
 
  // Main App
  var states = 5;
  var actions = 3;
- policy.buildQ(states, actions);
+ var agent = createAgent(states, actions);
 
 
